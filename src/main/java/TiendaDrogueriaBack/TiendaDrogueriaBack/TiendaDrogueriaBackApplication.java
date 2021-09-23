7a52f4d57416322bc13c065f80bb353d78103035
efcan <EduarOspina01>
2021-09-22 20:07:40 -0500

    Backend versión 1

---
+++ src/main/java/TiendaDrogueriaBack/TiendaDrogueriaBack/TiendaDrogueriaBackApplication.java
@@ -0,0 +1,13 @@
+package TiendaDrogueriaBack.TiendaDrogueriaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDrogueriaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDrogueriaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TiendaDrogueriaBack/TiendaDrogueriaBack/TiendaDrogueriaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaDrogueriaBack.TiendaDrogueriaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDrogueriaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
